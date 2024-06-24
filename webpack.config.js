877d99a022ec3ce51a889c8c37b1896b0bccf376
17070610 <schimea@hotmail.com>
2024-06-24 08:16:11 +0200

    Add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,22 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpeg|jpg|gif)$/i,
-            type: 'asset/resource',
-        },
-    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpeg|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ],
+    },    
     plugins : [
         new HtmlWebpackPlugin ({
             template:'./src/index.html',
