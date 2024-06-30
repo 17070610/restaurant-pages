aee6138605bdfec845a51755cac9ea4c81bd3e4f
17070610 <schimea@hotmail.com>
2024-06-30 10:38:01 +0200

    Add an image to page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './style.css';
 import Background from './background1.jpg';
 
-const content = document.getElementById('content');
+const content = document.getElementById('content')
+function welcome() {
+    const welcomeEl = document.createElement('div');
+    welcomeEl.classList.add('welcome');
+
+    const myBackgound = new Image();
+    myBackgound.src = Background;
+
+    welcomeEl.appendChild(myBackgound);
+
+    return welcomeEl;
+}
+
+document.body.appendChild(welcome());
