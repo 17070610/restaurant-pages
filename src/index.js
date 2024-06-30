bf134aa941a87df72dc08ec20ecc171f142afe2c
17070610 <schimea@hotmail.com>
2024-06-30 11:23:34 +0200

    Remove samples

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import './style.css';
 import Background from './background1.jpg';
 
-const content = document.getElementById('content')
-function welcome() {
-    const welcomeEl = document.createElement('div');
-    welcomeEl.classList.add('welcome');
-
-    const myBackgound = new Image();
-    myBackgound.src = Background;
-
-    welcomeEl.appendChild(myBackgound);
-
-    return welcomeEl;
-}
-
-document.body.appendChild(welcome());
